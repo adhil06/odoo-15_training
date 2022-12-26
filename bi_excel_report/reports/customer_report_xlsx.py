b5e0036cb80359b0148200bd9c359c2b3baf2daf
adhil06 <adhilkizhur@gmail.com>
2022-12-26 17:44:56 +0530

    all done

--- bi_excel_report/reports/customer_report_xlsx.py
+++ bi_excel_report/reports/customer_report_xlsx.py
@@ -10,53 +10,64 @@ class CustomerReportXlsx(models.AbstractModel):
     def generate_xlsx_report(self, workbook, data, lines):
     
            
-            # sheet.write(row,col+4,sale['price_unit'])
-            # sheet.write(row,col+5,sale['tax_id'])
-            # sheet.write(row,col+6,sale['price_subtotal'])
+           
         partner = data['partner_id']
+        date_fr =data['date_from']
+        date_t=data['date_to']
         sheet = workbook.add_worksheet('sales')  
         bold = workbook.add_format({'bold':True})
         sheet.set_column('B:B',13)
         sheet.set_column('C:C',13)
-        sheet.set_column('D:D',10)
+        sheet.set_column('D:D',12)
         sheet.set_column('E:E',11)
         sheet.set_column('F:F',15)
         sheet.set_column('G:G',10)
-
-        sale_orders=self.env['sale.order'].search([('partner_id','=',partner)])
-        # row=0
+        merge_format2=workbook.add_format({"bold":True,"color":"black"})
+        sheet.merge_range("A2:B2",f"from date:{date_fr}",merge_format2)
+        merge_format3=workbook.add_format({"bold":True,"color":"black"})
+        sheet.merge_range("C2:D2",f"to date:{date_t}",merge_format3)
         col=0
-        # sheet.write(row,col,'SALES',bold)
-        row=2
+            
+        domain = []
+        if partner:
+           domain+=[('partner_id','=',partner)] 
         if date_fr:
-            
-            
+            domain+=[('date_order','>=',date_fr)] 
+        if date_t:
+            domain+=[('date_order','<=',date_t)] 
+        #we can also directly give condition inside search like .search([('partner_id','=',partner),('date_order','>=',date_fr),('date_order','<=',date_t)])
+        sale_orders=self.env['sale.order'].search(domain)
+               
         merge_format1=workbook.add_format({"align":"center","bold":True,"color":"black"})
-        sheet.merge_range("A1:G1","SALE EXCEL SHEET",merge_format1)
-        sheet.write(3,0,'ID',bold)
-        sheet.write(3,1,'CUSTOMER',bold)
-        sheet.write(3,2,'ORDER DATE',bold)
-        sheet.write(3,3,'QUANDITY',bold)
-        sheet.write(3,4,'UNIT PRICE',bold)
-        sheet.write(3,5,'TAX',bold)
-        sheet.write(3,6,'SUBTOTAL',bold)
+        sheet.merge_range("A1:G1",f"SALE EXCEL SHEET -BassamInfoTech",merge_format1)
+        heading_format = workbook.add_format({'bold': True, 'color': 'gray'})
+        sheet.write(3,0,'ID',heading_format)
+        sheet.write(3,1,'CUSTOMER',heading_format)
+        sheet.write(3,2,'PRODUCT',heading_format)
+        sheet.write(3,3,'ORDER DATE',heading_format)
+        sheet.write(3,4,'QUANDITY',heading_format)
+        sheet.write(3,5,'UNIT PRICE',heading_format)
+        sheet.write(3,6,'TAX',heading_format)
+        sheet.write(3,7,'SUBTOTAL',heading_format)
         row=4
         for sale_order in sale_orders:
             row=row+1
             sheet.write(row,col,sale_order.name)
             sheet.write(row,col+1,sale_order.partner_id.name)
             format2 = workbook.add_format({'num_format': 'dd/mm/yy'})
-            sheet.write(row,col+2,sale_order.date_order,format2)
+            sheet.write(row,col+3,sale_order.date_order,format2)
             for line in sale_order.order_line:
-                sheet.write(row,col+3,line.product_uom_qty)
-                sheet.write(row,col+4,line.price_unit)
+                sheet.write(row,col+2,line.product_id.name)
+                sheet.write(row,col+4,line.product_uom_qty)
+                sheet.write(row,col+5,line.price_unit)
                 temp = row
                 for tax in line.tax_id:
-                    sheet.write(row,col+5,tax.name)
+                    sheet.write(row,col+6,tax.name)
                     temp=temp+1
-                sheet.write(row,col+6,line.price_subtotal)
+                sheet.write(row,col+7,line.price_subtotal)
                 row=row+1
-            sheet.write(row,col+6,sale_order.amount_total)   
+            sheet.write(row,col+6,'total')
+            sheet.write(row,col+7,sale_order.amount_total)   
                
                
     
--- bi_excel_report/wizard/sale_excel_report_wizard.py
+++ bi_excel_report/wizard/sale_excel_report_wizard.py
@@ -5,7 +5,7 @@ class SaleExcelReportWizard(models.TransientModel):
     _name="sale.excel.report.wizard"
     _description ="Sale excel report wizard"
 
-    name = fields.Many2one(comodel_name="res.partner",string="Name",required=True)
+    name = fields.Many2one(comodel_name="res.partner",string="Customer")
     date_from = fields.Date(string="Date From")
     date_to = fields.Date(string="Date To")
     
@@ -14,8 +14,8 @@ class SaleExcelReportWizard(models.TransientModel):
         
         data = {
                 'partner_id':self.name.id,
-                'date_fr':self.date_from,
-                'date_t':self.date_to
+                'date_from':self.date_from,
+                'date_to':self.date_to
                 
             }
         return self.env.ref('bi_excel_report.customer_excel_report_xlsx').report_action(self, data=data, config=False)
